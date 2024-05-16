d48678c33251551ab076fb60c5b40f60053c1c4e
Gustavo Roos <gustavohroos@gmail.com>
2024-05-16 11:44:30 -0300

    feat: add route to list all openings

--- router/routes.go
+++ router/routes.go
@@ -5,17 +5,30 @@ import "github.com/gin-gonic/gin"
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "GET Opening",
+			})
+		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "POST Opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "DELETE Opening",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "PUT Opening",
+			})
+		})
 		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"message": "Opening"})
-		})
-		v1.POST("/openings", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"message": "Opening"})
-		})
-		v1.DELETE("/openings", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"message": "Opening"})
-		})
-		v1.PUT("/openings", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"message": "Opening"})
+			ctx.JSON(200, gin.H{
+				"message": "GET Openings",
+			})
 		})
 
 	}
